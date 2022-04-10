184a8efabe24f6e83a9f7735e545eff957d9804d
ramdhani <iqbal.ramdhani@dhealth.co.id>
2022-04-10 14:20:28 +0700

    perbaikan code

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'srv143.niagahoster.com',
-	'username' => 'u1653714_keuangan',
-	'password' => '&#w;Ouw73~5(',
-	'database' => 'u1653714_keuangan',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'keuangan_smk',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
